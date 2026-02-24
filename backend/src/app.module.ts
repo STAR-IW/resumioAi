fd1ea47e9cacc59c9d82e4725414ac9ef40f38e0
Itai <starnes.i@gmail.com>
2026-02-25 00:59:56 +0200

    job module, job endpoint

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UploadModule } from './upload/upload.module';
+import { JobModule } from './job/job.module';
 
 @Module({
-  imports: [UploadModule],
+  imports: [UploadModule, JobModule],
   controllers: [AppController],
   providers: [AppService],
 })
