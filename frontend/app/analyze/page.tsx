ef7e2d3b6c1d8a351eb0aae74476566ed72663b8
Itai <itai@empyreanmed.com>
2026-03-23 15:39:44 +0200

    Generating response from llm

--- frontend/app/analyze/page.tsx
+++ frontend/app/analyze/page.tsx
@@ -2,16 +2,33 @@
 import FileUpload from "@/components/FileUpload";
 import JobInput from "@/components/JobInput";
 import {useState} from "react";
+import {Button} from "@/components/ui/button";
+
+
 
 export default function Analyze(){
     const [cvText, setCvText] = useState<string>('');
+    const [jobInput, setJobInput] = useState<string>('');
+    const [analyzeResult, setAnalyzeResult] = useState<string>('');
+
+    async function handleAnalyze( ){
+
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/analyze`, {
+            method: "POST",
+            body: JSON.stringify({ cvText, jobDescription: jobInput }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        const data = await res.json();
+        setAnalyzeResult(data);
+        console.log(data);
+    }
 
     return (
         <div className='container mx-auto p-8'>
             <div className="grid grid-cols-2 gap-8">
                 <FileUpload onUpload={(cvText : string)=>{ console.log('cvText:', cvText); setCvText(cvText); }}></FileUpload>
-                <JobInput></JobInput>
-
+                <JobInput   onJobInput = {(jobInput: string) =>{console.log('jobInput:', jobInput), setJobInput(jobInput)} }></JobInput>
+                <Button onClick={handleAnalyze} disabled={!cvText || !jobInput}>Analyze</Button>
             </div>
         </div>
         )
