4734db3bbb6bddea33a73ec25e810fea480f2365
Itai <starnes.i@gmail.com>
2026-05-20 19:04:24 +0300

    fix:add type annotation to hexToRgb parameter

--- frontend/components/Grainient.tsx
+++ frontend/components/Grainient.tsx
@@ -4,7 +4,7 @@ import { useEffect, useRef } from 'react';
 import { Renderer, Program, Mesh, Triangle } from 'ogl';
 import './Grainient.css';
 
-const hexToRgb = hex => {
+const hexToRgb =  (hex: string) => {
     const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     if (!result) return [1, 1, 1];
     return [parseInt(result[1], 16) / 255, parseInt(result[2], 16) / 255, parseInt(result[3], 16) / 255];
